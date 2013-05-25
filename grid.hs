2da3566cdfacada09f17d51442cabbe2616cebe4
Simon Horlick <simonhorlick@gmail.com>
2013-05-25 19:44:56 +0100

    Drawing code for grids
    Grids are drawn using triangle strips in GL. You can render a whole
    tessalated grid in a single draw call if you're careful with your
    indices. This takes the approach of rendering a row of right angle
    triangles then a mirrored row of right angle triangles below it, this is
    required to be able to traverse everything in a single draw call.
    
    At the end of each row, three degenerate triangles must be rendered to
    keep the winding order consistent.
    
    See
    http://dan.lecocq.us/wordpress/2009/12/25/triangle-strip-for-grids-a-construction/
    for a better explanation.

---
+++ grid.hs
@@ -0,0 +1,15 @@
+-- Zip the lists a and b, concatenating into a single list
+flatZip a b = concat $ zipWith (\x y -> [x,y]) a b
+
+-- Grids are drawn as OpenGL triangle strips. The vertices are traversed in the
+-- order v00 v10 v01 v11. The indices into the vertex buffer then become 0, w,
+-- 1, (w+1), 2, (w+2), ..., (w-1), (2*w-1). At the end of a row we must draw
+-- some degenerate triangles to keep the winding order consistent.
+gridRowTriStripIndices w =
+    flatZip [0..w-1] [w..(2*w-1)]
+    ++ [2*w-1,2*w-1] -- fix winding order
+    ++ flatZip [3*w-1,3*w-2..2*w+1] [2*w-2,2*w-3..w]
+    ++ [2*w,2*w,2*w] -- fix winding order again
+
+gridTriStripIndices w h = gridRowTriStripIndices w
+
