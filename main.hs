49318c65c64664cafc0918e6582047d1683abab7
Simon Horlick <simonhorlick@gmail.com>
2013-05-26 00:27:48 +0100

    Render the grid with basic transform and lighting

--- main.hs
+++ main.hs
@@ -3,6 +3,10 @@ module Main where
 import Graphics.GPipe
 import Graphics.UI.GLUT( Window, mainLoop, idleCallback, getArgsAndInitialize, ($=) )
 
+import Grid
+
+import qualified Data.Vec as Vec
+
 main :: IO ()
 main = do
   -- Setup GLUT
@@ -21,9 +25,54 @@ main = do
 -- Called by GLUT each frame
 renderFrame :: Vec2 Int -> IO (FrameBuffer RGBFormat () ())
 renderFrame size = do
-  return $ newFrameBufferColor (RGB (0.1:.0.3:.0.6:.()))
+  return $ draw (rasteriseTriangle size) clear
+  where
+    draw  = paintColor NoBlending (RGB $ Vec.vec True)  
+    clear = newFrameBufferColor (RGB (Vec.fromList [0.1,0.3,0.6]))
 
 -- Set up GLUT callbacks
 initWindow :: Window -> IO ()
 initWindow win = idleCallback $= Nothing
+
+
+-- This implements the fragment shader
+ 
+rasteriseTriangle :: Vec2 Int -> FragmentStream (Color RGBFormat (Fragment Float))
+rasteriseTriangle size = fmap (\(front,color) -> RGB color) $ rasterizeFrontAndBack $ procTriangle size 
+
+
+-- This implements the vertex shader
+
+procTriangle :: Vec2 Int -> PrimitiveStream Triangle (Vec4 (Vertex Float), Vec3 (Vertex Float))
+procTriangle size = fmap (projTriangle size) gridStream 
+
+-- Compute the projection matrix for the triangle
+projTriangle :: Vec2 Int -> Vec3 (Vertex Float) -> (Vec4 (Vertex Float), Vec3 (Vertex Float))
+projTriangle size pos = (orthoProj `multmv` homPos, toGPU $ 0:.1:.0:.())
+  where homPos = homPoint pos :: Vec4 (Vertex Float)  
+        orthoProj = toGPU $ orthogonal (-10) 10  (2:.2:.())
+
+gridVertices = 
+  [
+    0.0:.0.0:.0.0:.(),
+    0.1:.0.0:.0.0:.(),
+    0.2:.0.0:.0.0:.(),
+    0.3:.0.0:.0.0:.(),
+    0.0:.0.1:.0.0:.(),
+    0.1:.0.1:.0.0:.(),
+    0.2:.0.1:.0.0:.(),
+    0.3:.0.1:.0.0:.(),
+    0.0:.0.2:.0.0:.(),
+    0.1:.0.2:.0.0:.(),
+    0.2:.0.2:.0.0:.(),
+    0.3:.0.2:.0.0:.(),
+    0.0:.0.3:.0.0:.(),
+    0.1:.0.3:.0.0:.(),
+    0.2:.0.3:.0.0:.(),
+    0.3:.0.3:.0.0:.()
+  ]
+
+-- Create a triangle stream describing a tesselated grid
+gridStream :: PrimitiveStream Triangle (Vec3 (Vertex Float))
+gridStream = toIndexedGPUStream TriangleStrip gridVertices (gridTriStripIndices 4 4)
 
