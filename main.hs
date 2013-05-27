4362f5c872a502a1ed85ca5e2c0b1718ab8948a5
Simon Horlick <simonhorlick@gmail.com>
2013-05-27 12:11:56 +0100

    Pass window size to perspective projection
    The window width and height is required to calculate the aspect ratio
    when the window is resized.

--- main.hs
+++ main.hs
@@ -42,18 +42,19 @@ type Normal = Vec3 (Vertex Float)
 
 -- Default fragment shader
 rasterise :: Vec2 Int -> FragmentStream (Color RGBFormat (Fragment Float))
-rasterise size = fmap (\(front, color) -> RGB color) $ rasterizeFrontAndBack $ transformVertices
+rasterise size = fmap (\(front, color) -> RGB color) $ rasterizeFrontAndBack (transformVertices size)
 
 -- Apply model view projection matrices to each vertex in the stream
-transformVertices :: PrimitiveStream Triangle (Position', Normal)
-transformVertices = fmap (transformVertex) $ gridStream
+transformVertices :: Vec2 Int -> PrimitiveStream Triangle (Position', Normal)
+transformVertices size = fmap (transformVertex size) $ gridStream
 
 -- Default hardware transform
-transformVertex :: (Position, Normal) -> (Position', Normal)
-transformVertex (pos,norm) = (transformedPos,norm)
+transformVertex :: Vec2 Int -> (Position, Normal) -> (Position', Normal)
+transformVertex (width:.height:.()) (pos,norm) = (transformedPos,norm)
     where
          viewMat = (translation (0:.0:.(-2):.())) `multmm` (rotationX (pi/6)) `multmm` (rotationY (pi/4))
-         projMat = perspective 1 100 (pi/3) (1280.0 / 720.0)
+         projMat = perspective 1 100 (pi/3) aspectRatio
+         aspectRatio = (fromIntegral width) / (fromIntegral height)
          viewProjMat = projMat `multmm` viewMat
          transformedPos = toGPU viewProjMat `multmv` (homPoint pos :: Vec4 (Vertex Float))
 
